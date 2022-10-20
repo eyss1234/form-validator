96c07a8c4abf4b85bb42adad0398402e8c4a126e
Emily Santos <eyssantos@gmail.com>
2022-10-20 05:57:26 +0100

    js select elements

---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.querySelector('password2')
+const messageContainer
