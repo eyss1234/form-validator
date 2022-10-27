17a7d4855c0d8befdd5f145829161ce9561d6846
Emily Santos <eyssantos@gmail.com>
2022-10-27 16:58:57 +0100

    form validation check

--- script.js
+++ script.js
@@ -4,11 +4,22 @@ const password2El = document.getElementById('password2')
 const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
+let isValid = false;
+
+function validateForm() {
+  // using Constraint API
+  isValid = form.checkValidity();
+  // style main message for an error
+  message.textContent = 'Please fill out all fields';
+  message.style.color = 'red';
+  messageContainer.style.borderColor = 'red';
+}
+
 // Submit event
 function processFormData(e) {
   e.preventDefault();
-  console.log(e);
-
+  // Validate form
+  validateForm();
 }
 
 // Event Listener
