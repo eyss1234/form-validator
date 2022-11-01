ce01042b9c1452fed6581407ecf3a4eaba8f16d4
Emily Santos <eyssantos@gmail.com>
2022-11-01 13:32:50 +0000

    check if both passwords match

--- script.js
+++ script.js
@@ -10,10 +10,26 @@ function validateForm() {
   // using Constraint API
   isValid = form.checkValidity();
   // style main message for an error
-  message.textContent = 'Please fill out all fields';
-  message.style.color = 'red';
-  messageContainer.style.borderColor = 'red';
+  if (isValid) {
+    message.textContent = 'Please fill out all fields';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+  }
+  // check to see if passwords match
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColor = 'green';
+    password2El.style.borderColor = 'green';
+  } else {
+    passwordsMatch = false;
+    message.textContent = 'Make sure passwords match';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    password1El.style.borderColor = 'red';
+    password2El.style.borderColor = 'red';
+  }
 }
+
 
 // Submit event
 function processFormData(e) {
